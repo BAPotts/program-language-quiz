f9433efe90a29a0ba572e30bf435a70b82699ce5
Beverly Potts <pottsbeverly@gmail.com>
2020-06-05 14:12:24 -0400

    Remove result alerts and replace with .text method within conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+    event.preventDefault();
     const animal = $("select#animal").val();
     const schedule = $("select#schedule").val();
     const travel = $("select#travel").val();
     const hairColor = $("select#hair-color").val();
     const tvShow = $("select#tv-show").val();
+    
 
-    if (animal === "lemur" && schedule === "up-late" && travel != "paris" && hairColor === "blonde"){
-      alert("Ruby");
+    if (animal === "lemur" && schedule === "up-late" &&  travel != "paris" && hairColor === "blonde"){
+      $("#output").text("You got Ruby!");
     } else if ((animal === "blob" && (schedule === "up-early" || schedule === "up-late") && travel === "rome") || (hairColor === "red")){ 
-      alert("Python");
-    }else { alert("C#");
+      $("#output").text("You got Python!");
+    }else { $("#output").text("You got c#!");
     }
 
 
